4c424e7f605133ec09edcd62fc93b527bdba93e1
emily-1487 <emilyliu11022011@gmail.com>
2023-06-30 21:31:13 +0800

    hw3

--- class3/hw3.py
+++ class3/hw3.py
@@ -1,12 +1,15 @@
 '''
 1. 請問使用什麼function可以顯示出形態
-答案:
+答案:type
 2.請問今天學了哪一些function(函式)?
-答案:
+答案:len type int float bool str input
 3. 延續上題, 請嘗試描述每個function的功能各別是什麼?
-答案:
+答案:len = 算字串長度 type = 取得資料物件的型態 int = 將輸入數值轉換為整數型態  float=將輸入數值轉換為小數 bool=將輸入數值轉為布林 str=將輸入數值轉為字串 input=提示使用者輸入資料
 4. 請使用者輸入自己的年齡(input)
 程式會顯示出(print)
 你的年齡是
 XX
 '''
+a = input("輸入自己的年齡:")
+print("你的年齡是")
+print(a)
