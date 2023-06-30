7c002061f67fd2fa379d87c5ab3ddca048df6d73
emily-1487 <emilyliu11022011@gmail.com>
2023-06-30 21:21:45 +0800

    class3

---
+++ class3/class3.py
@@ -0,0 +1,31 @@
+'''
+# 函數:
+# len = 算字串長度
+# type = 取得資料物件的型態
+# int = 將輸入數值轉換為整數型態
+# float=將輸入數值轉換為小數
+# bool=將輸入數值轉為布林
+# str=將輸入數值轉為字串
+# e=10的幾次方
+# input=提示使用者輸入資料
+# 輸出返回:使用者輸入的資料,字串型態
+'''
+#ctrl+?=快速註解
+# print(False)
+# print(-1.000)
+# print(123)
+# print("hello world")
+# print("可以在終端機顯示文字")
+# print("之前學過的東西可以印出來")
+# a = "hello"
+# b = "world"
+# print(a + "" + b)
+# print("開始輸入")
+# s = input()  # 是字串
+# print("輸入結束")
+# print(type(s))
+# print(s + "是小天才")  # 因為input是字串所以可以使用字串加法
+# print(s * 10)
+a = input("輸入資料:")
+# input("提示使用者的文字")
+print(a)
