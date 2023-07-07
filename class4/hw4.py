aecbbaece0752bb88dd451081ca478aa910e5f59
emily-1487 <emilyliu11022011@gmail.com>
2023-07-07 21:11:06 +0800

    hw4.py

---
+++ class4/hw4.py
@@ -0,0 +1,27 @@
+"""
+Topic:請使用input and print打造對話機器人
+e.g.
+old = input("How old are you?")
+print("I am " + old)
+1.Show:How old are you?
+2.input:12
+3.Output:I am 12
+"""
+a=input("How old are you?")
+
+
+
+
+
+
+
+"""
+請使用者輸入身高(公尺)h以及體重(公斤)w
+透過下面公式計算出BMI數值並顯示計算結果
+bmi = w/h**2
+EX:
+請輸入身高:1.7
+請輸入體重:50
+你的BMI為17.301038062283737
+"""
+w=50公斤
