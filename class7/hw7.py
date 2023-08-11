62c8a5262e69cb5ec37827f1c385d662e31bcc83
emily-1487 <emilyliu11022011@gmail.com>
2023-08-11 20:01:52 +0800

    hw7.py

--- class7/hw7.py
+++ class7/hw7.py
@@ -11,15 +11,14 @@ E : 59 分以下
 ex:
 例如:使用者輸入成績50 顯示E。使用者輸入62則顯示D
 '''
-s=input("請輸入成績")
-if s>="90":  
+s = int(input("請輸入成績:"))
+if s >= 90:
     print("你的等級為A")
-elif s>="80"and<="89":
+elif s >= 80 and s <= 89:
     print("你的等級為B")
-elif s>="70"and<="79":
+elif s >= 70 and s <= 79:
     print("你的等級為C")
-elif s>="60"and<="69":
+elif s >= 60 and s <= 69:
     print("你的等級為D")
 else:
-    s<="59":
     print("你的等級為E")
