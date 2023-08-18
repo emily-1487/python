53167e1c3fe0dcf7ce6a4c100df2880e6929c687
emily-1487 <emilyliu11022011@gmail.com>
2023-08-18 20:55:14 +0800

    class9

---
+++ class9/class9.py
@@ -0,0 +1,21 @@
+import turtle
+
+# turtle.forward(150)
+# turtle.left(60)
+# turtle.forward(150)
+# turtle.left(60)
+# turtle.forward(150)
+# turtle.left(60)
+# turtle.forward(150)
+# turtle.left(60)
+# turtle.forward(150)
+# turtle.left(60)
+# turtle.forward(150)
+# turtle.left(60)
+# turtle.done()
+# loof=迴圈
+# for 迴圈變數 in range(執行次數):要重複的程式
+#hello hello hello hello hello
+# for 迴圈變數 in range(起始值,終止值,變化值):要重複的程式
+for i in range(2, 10, 2):
+    print(i)
