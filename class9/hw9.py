6a741c366d25ada32d9df72ef139f3e87f76286d
emily-1487 <emilyliu11022011@gmail.com>
2023-08-23 18:47:56 +0800

    class9

---
+++ class9/hw9.py
@@ -0,0 +1,21 @@
+'''
+作業1.
+使用turtle模組與for 劃出一個正六邊形
+作業2.
+做一個可以列印出0-100之間3的倍數的機器。
+ex:
+3
+6
+9
+12
+....
+'''
+import turtle
+for i in range(0, 6):
+    turtle.forward(150)
+    turtle.left(60)
+print(i)
+turtle.done()
+
+for i in range(3, 100, 3):
+    print(i)
