2cdd22a18d419b4d6b2d5ece1690d32d4e16e0bc
emily-1487 <emilyliu11022011@gmail.com>
2023-08-29 13:53:46 +0800

    class10

--- class10/hw10.py
+++ class10/hw10.py
@@ -3,16 +3,14 @@ import turtle
 
 turtle.color("black")
 turtle.penup()
-
 for i in range(8):
     turtle.forward(100)
     turtle.stamp()
     turtle.backward(100)
     turtle.left(45)
-    print(i)
 turtle.home()
 turtle.done()
-# homework-2
+# hw-2
 import turtle
 import time
 
