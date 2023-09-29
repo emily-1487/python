73f604bbd3dde88f43fa4cb18040752440c39518
emily-1487 <emilyliu11022011@gmail.com>
2023-09-29 22:30:22 +0800

    class14

--- class14/hw14.py
+++ class14/hw14.py
@@ -1,12 +1,45 @@
+'''
+當使用者輸入數值時，程式不僅提示再大再小還需要提示縮小過後的輸入範圍
+EX:
+請輸入0~100的整數:50
+再小一點
+請輸入0~50的整數:25
+再小一點
+請輸入0~25的整數:15
+再大一點
+請輸入15~25的整數:30
+再小一點
+請輸入15~25的整數:10
+再大一點
+請輸入15~25的整數:20
+再大一點
+請輸入20~25的整數:23
+再大一點
+請輸入23~25的整數:24
+恭喜猜中!
+'''
 import random
 
-s = random.randrange(1, 100)
+a = random.randint(1, 100)
+s = 1
+d = 100
 while True:
-    a = int(input("請輸入1~100的整數:"))
-    if a == s:
-        print("恭喜答對!")
+    z = str(s)
+    x = str(d)
+    i = int(input("請輸入" + z + "~" + x + "的數:"))
+    if i == a:
+        print("恭喜猜中")
         break
-    elif a < s:
-        print("在大一點")
-    elif a > s:
-        print("在小一點")
+    elif i > a:
+        print("再小一點")
+        if i > d:
+            continue
+        elif i < d:
+            d = i
+        continue
+    elif i < a:
+        print("再大一點")
+        if i < s:
+            continue
+        elif i > s:
+            s = i
