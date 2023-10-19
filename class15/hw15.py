03966bd8b09d77929038e4053e4ace625507e93d
emily-1487 <emilyliu11022011@gmail.com>
2023-10-19 20:06:54 +0800

    class15

--- class15/hw15.py
+++ class15/hw15.py
@@ -5,4 +5,18 @@ hw15
 juices_list = ["蘋果汁","柳橙汁","葡萄汁","系統關閉"]
 '''
 while True:
-    
+    L = ["1.蘋果汁", "2.柳橙汁", "3.葡萄汁", "4.系統關閉"]
+    for i in range(len(L)):
+        print(L[i])
+    a = input("請輸入編號:")
+    if a == "1":
+        print("蘋果汁")
+    elif a == "2":
+        print("柳橙汁")
+    elif a == "3":
+        print("葡萄汁")
+    elif a == "4":
+        print("系統關閉")
+        break
+    else:
+        print("沒有此果汁")
