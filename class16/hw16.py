a841e9bf539263195f4e109ee2962879fedcee9e
emily-1487 <emilyliu11022011@gmail.com>
2023-10-27 21:01:27 +0800

    class16

--- class16/hw16.py
+++ class16/hw16.py
@@ -8,6 +8,7 @@
 3. 查詢餐點在點餐清單中的位置
 4. 退出點餐機
 '''
+A_list = []
 while True:
     print("1. 新增餐點到點餐清單")
     print("2. 從點餐清單中移除特定餐點")
@@ -17,14 +18,37 @@ while True:
     if a == '1':
         # 新增餐點到點餐清單的程式碼
         print("您選擇了新增餐點到點餐清單功能")
+        new_list = input("請輸入新的菜單")
+        A_list.append(new_list)
+        print("以新增餐點到點餐清單")
     elif a == '2':
         # 從點餐清單中移除特定餐點的程式碼
         print("您選擇了從點餐清單中移除特定餐點功能")
+        number_list = input("輸入要移除的餐點:")
+        if number_list in A_list:
+            A_list.remove(number_list)
+            print("已從點餐清單中移除特定餐點!")
+        else:
+            print("該餐點不再餐點清單中")
     elif a == '3':
-        # 查詢餐點在點餐清單中的位置的程式碼
-        print("您選擇了查詢餐點在點餐清單中的位置功能")
+        p = int(input("請輸入你想要插入的餐點:"))
+        order = input("請輸入你想要加入的餐點:")
+        A_list.insert(p, order)
+        print("已在指定位置加入餐點!")
     elif a == '4':
-        print("感謝您使用點餐機，祝您用餐愉快！")
-        break
-    else:
-        print("請輸入有效的選項！")
+        order = input("請輸入你想要計算的餐點:")
+        count = A_list.count(order)
+        print("餐點" + order + "在餐點清單中的數量是:" + str(count))
+    elif a == "5":
+        if A_list:
+            A_list.pop()
+            print("已取消點餐清單中的最後一項餐點!")
+        else:
+            print("點餐清單是空的!")
+    elif a == "6":
+        p = int(input("請輸入你想要取消的餐點位置:"))
+        if 0 <= p < len(A_list):
+            A_list.pop(p)
+            print("已取消點餐清單中特定位置的餐點!")
+        else:
+            print("輸入位置不正確~")
---
+++ class17/class17.py
@@ -0,0 +1,21 @@
+# append=將元素增加到串列的最後
+# l = [1, 2, 3]
+# l.append(5)
+# print(l)
+# remove=移除串列中的元素
+#        指定的元素必須存在
+#        移除第一個出現的元素
+# l = ["a", "b", "c", "a"]
+# l.remove("a")
+# print(l)
+# insert=將元素插入指定的位置
+# l = [1, 2, 3]
+# l.insert(1, "SB")
+# print(l)
+# count=計算特定元素在串列中的數量
+# l = [9, 1, -4, 3, 7, 11, 3]
+# print(l.count(3))
+# pop=移除指定索引的元素
+# l = [1, 2, 3]
+# l.pop()
+# print(l)
