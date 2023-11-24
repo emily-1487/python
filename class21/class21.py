b1ff3c523245e659a93ed82ae7aee265d36e9e7c
emily-1487 <emilyliu11022011@gmail.com>
2023-11-24 21:16:09 +0800

    class21

---
+++ class21/class21.py
@@ -0,0 +1,15 @@
+# def hello():
+#     print("hello")
+# # hello()
+# apple=["a","v"]
+# len(apple)
+# def hello(name):
+#     print("Hello"+str(name))
+# hello("47")
+# def my_min(a,b):
+#     if a<b:
+#         return a
+#     else:
+#         return b
+# print(my_min(2,1))
+global=
---
+++ class21/hw21.py
@@ -0,0 +1,11 @@
+'''
+製作一個丟骰子的指令
+使用者可以輸入擲骰子的次數n
+指令會一次回傳所有擲骰子的結果
+程式內容前兩行打成
+import random
+def roll_dice(n):
+'''
+import random
+def roll_dice(n):
+    
