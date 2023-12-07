51b259f23ccc393f2f54e97a65891aea82f42ab0
emily-1487 <emilyliu11022011@gmail.com>
2023-12-07 22:27:41 +0800

    class22

---
+++ class22/hw22.py
@@ -0,0 +1,22 @@
+# x=0
+# while x<4:
+#     if x%4==0:
+#         print("party")
+#     elif x-2<0:
+#         print("cake")
+#     elif x-3==0:
+#         print("greeting")
+#     else:
+#         print("birthday")
+#     x=x+1
+# a="test1"
+# print(a)
+# b="test2"
+# a+=b
+# print(a)
+# print(b)
+x="Tiger"
+y="Lion"
+z="Juguar"
+a="{1} and {0} and {2}"
+print (a.format(x,y,z))
