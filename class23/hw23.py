6252bf28d32880ec3cbd0528ca01d4ed836bf83a
emily-1487 <emilyliu11022011@gmail.com>
2023-12-14 22:03:10 +0800

    class23

---
+++ class23/hw23.py
@@ -0,0 +1,22 @@
+# a=["a","b","c","d","e"]
+# b=[1,2,3,4,5]
+# print(a is b)
+# print(a==b)
+# a=b
+# print(a is b)
+# print(a==b)
+
+# p=2
+# n=5
+# while n!=0:
+#     p*=n
+#     print(p)
+#     n-=1
+#     if n==2:break
+
+# import datetime
+# d=datetime.datetime(2018,5,16)
+# print("{:%m-%d-%y}".format(d))
+# num=1234567.89
+# print("{:,.2f}".format(num))
+
